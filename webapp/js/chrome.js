f0c2ab83c411e5691de2a53a7911cf1dec3af012
Aaron Surty <aaron.surty@gmail.com>
2013-05-05 21:59:23 -0400

    todos

--- webapp/js/chrome.js
+++ webapp/js/chrome.js
@@ -1,7 +1,7 @@
 define(['jquery'], function($) {
     $(function() {
         $('#mover_background p').css({
-            'font-size':$(window).height()/3*2+'px'
+            'font-size':$(window).height()/4*3+'px'
         });
     });
 });
